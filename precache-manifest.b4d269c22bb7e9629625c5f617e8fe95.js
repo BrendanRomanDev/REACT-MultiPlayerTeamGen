d10346ccf84b26a6e4b5d688f02cf5b7943348ab
Brendan Roman <brendanroman93@gmail.com>
2020-06-26 23:23:06 -0400

    Updates

---
+++ precache-manifest.b4d269c22bb7e9629625c5f617e8fe95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc494c7af40a132d850e9deacf0fc1a7",
+    "url": "/REACT-MultiPlayerTeamGen/index.html"
+  },
+  {
+    "revision": "a2f0eb70693e091d4161",
+    "url": "/REACT-MultiPlayerTeamGen/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ac2945c54cbdf2367b9d",
+    "url": "/REACT-MultiPlayerTeamGen/static/css/main.819920d1.chunk.css"
+  },
+  {
+    "revision": "a2f0eb70693e091d4161",
+    "url": "/REACT-MultiPlayerTeamGen/static/js/2.11c7851a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/REACT-MultiPlayerTeamGen/static/js/2.11c7851a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2945c54cbdf2367b9d",
+    "url": "/REACT-MultiPlayerTeamGen/static/js/main.f1535ae4.chunk.js"
+  },
+  {
+    "revision": "67c8da0c133b710f1fc6",
+    "url": "/REACT-MultiPlayerTeamGen/static/js/runtime-main.ddbe5553.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/REACT-MultiPlayerTeamGen/static/media/logo.5d5d9eef.svg"
+  }
+]);
